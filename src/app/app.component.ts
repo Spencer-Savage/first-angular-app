0ddc8183a316769f551df2fb3fec6881709c5180
Spencer Savage <email.spencersavage@gmail.com>
2025-03-16 19:55:15 -0500

    feat: add user component with dynamic user selection and styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
+import { UserComponent } from "./components/user/user.component";
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, UserComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
