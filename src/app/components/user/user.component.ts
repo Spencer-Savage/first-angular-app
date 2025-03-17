f5ad0f90342f388fa3a9925160aa8a8a24d1eb06
Spencer Savage <email.spencersavage@gmail.com>
2025-03-16 21:40:54 -0500

    feat: add Output and emit to user component for click event

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,19 +1,22 @@
-import { Component, Input} from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-user',
   imports: [],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
 })
 export class UserComponent {
-  @Input({required: true}) avatar!: string;
-  @Input({required: true}) name!: string;
+  @Input({ required: true }) id!: string;
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
+  @Output() select = new EventEmitter();
 
   get imagePath() {
     return `assets/users/${this.avatar}`;
   }
   onSelectUser() {
-    
+    this.select.emit(this.id);
   }
+  
 }
